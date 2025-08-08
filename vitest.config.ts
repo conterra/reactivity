6e0435afd02df0caa255d1ed0d969a7596740b30
Michael Beckemeyer <m.beckemeyer@conterra.de>
2025-08-08 12:53:01 +0200

    Fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,9 @@
 // SPDX-FileCopyrightText: 2024-2025 con terra GmbH (https://www.conterra.de)
 // SPDX-License-Identifier: Apache-2.0
-import esbuild from "rollup-plugin-esbuild";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    test: {},
-    plugins: [
-        // for decorators
-        esbuild()
-    ]
+    test: {
+        projects: ["packages/*"]
+    }
 });
