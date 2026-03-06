32271b8a09cbc44c03dd1e874e75626890684173
Michael Beckemeyer <m.beckemeyer@conterra.de>
2026-03-06 07:43:00 +0100

    Fix linter error

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,3 +1,4 @@
 // SPDX-FileCopyrightText: 2024-2025 con terra GmbH (https://www.conterra.de)
 // SPDX-License-Identifier: Apache-2.0
 /// <reference types="vite/client" />
+void 0; // Workaround for linter error
