c0bb11c79ce9ed1fd8b85d8dac849ebf68cb542f
Michael Beckemeyer <m.beckemeyer@conterra.de>
2026-05-22 13:31:18 +0200

    Run prettier

--- packages/reactivity-core/src/struct/struct.ts
+++ packages/reactivity-core/src/struct/struct.ts
@@ -88,8 +88,8 @@ type AnyFunc = (...args: any[]) => any;
  */
 type GetMemberSchemaForProp<T, V> = [V] extends [AnyFunc]
     ? // V is wrapped in an array to prevent distributive conditional types
-      // see https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
-      | PropertyMemberType
+          // see https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+          | PropertyMemberType
           | MethodMemberType<T, Parameters<V>, ReturnType<V>>
           | ComputedMemberType<T, V>
     : PropertyMemberType | ComputedMemberType<T, V>;
